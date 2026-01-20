2a6157007e82e6ae63a12db1de7992e3c52f5902
Justin Friesen <justinjfriesen@gmail.com>
2026-01-20 15:49:55 +0000

    Wrote code for the game logic
    Wrote code to compare the human choice to the computer choice and
    determine who won and update the score. Still don't think the code is
    fully working. Pushing this update to store the changes I've made for
    today.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-console.log("Hello World");
-
+//Computer Logic
 function getComputerChoice(min, max) {
     return Math.random() * (4 - 1) + 1;
 };
@@ -11,13 +10,55 @@ if (getComputerChoice() < 2) {
 } else {
     console.log("scissors");
 };
+//End of Computer Logic
 
+//Human Logic
 function getHumanChoice() {
     let humanChoice = prompt("What's your choice?");
     return humanChoice;
 }
 
 console.log(getHumanChoice());
+//End of Human Logic
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playGame() {
+
+    function playRound(humanChoice, computerChoice) {
+        humanChoice.toLowerCase
+
+        if ((humanChoice === "rock") && (computerChoice === "rock")) {
+            console.log("It's a tie! Try again?");
+        } else if ((humanChoice === "rock") && (computerChoice === "paper")) {
+            console.log("You lose! Try again!");
+            computerScore++;
+        } else if ((humanChoice === "rock") && (computerChoice === "scissors")) {
+            console.log("You Win! Try again?");
+            humanScore++;
+        } else if ((humanChoice === "paper") && (computerChoice === "rock")) {
+            console.log("You Win! Try again?");
+            humanScore++;
+        } else if ((humanChoice === "paper") && (computerChoice === "paper")) {
+            console.log("It's a tie! Try again?");
+        } else if ((humanChoice === "paper") && (computerChoice === "scissors")) {
+            console.log("You lose! Try again!");
+            computerScore++;
+        } else if ((humanChoice === "scissors") && (computerChoice === "rock")) {
+            console.log("You lose! Try again!");
+            computerScore++;
+        } else if ((humanChoice === "scissors") && (computerChoice === "paper")) {
+            console.log("You Win! Try again?");
+            humanScore++;
+        } else if ((humanChoice === "scissors") && (computerChoice === "scissors")) {
+            console.log("It's a tie! Try again?");
+        } else {
+            console.log("Please enter rock, paper or scissors.");
+        }
+    }
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+}
